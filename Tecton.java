b2a5594e9f3806e596b035c56e27f34376d91e70
Pásztor Gergő <pasztorg05@gmail.com>
2025-03-21 10:19:15 +0100

    Tecton class method declarations

--- Tecton.java
+++ Tecton.java
@@ -1,6 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Tekton osztály amely a tektonok helyesműködéséért felelős.
 */
 public class Tecton {
-    
+    private ArrayList<Insect> insects;
+    private MushroomBody mushroomBody;
+    private ArrayList<Spore> spores;
+    private ArrayList<String> strings; /*Itt majd figyrelni, hogy a mi String osztályunk legyen!*/
+    private ArrayList<Tecton> neighbours;
+    private int isze;
+
+    public Tecton(){}
+    public void cutString(String s){
+
+    }
+    public boolean canBranch(){
+
+    }
+    public int neighbourDistance(Tecton t){
+
+    }
+    public void scatterSpore(Spore s){
+
+    }
+    public void growBody(){
+
+    }
+    public boolean haveSpore(){
+
+    }
+    public void removeStrings(){
+
+    }
+    public void Break(int size){
+
+    }
+    public void setSize(){
+
+    }
+    public boolean canMoveTo(Tecton t){
+
+    }
+    public void setBugPosition(Insect i){
+
+    }
+    public void setMushroomBody(MushroomBody mb){
+
+    }
+    public void setInsect(Insect i){
+
+    }
+    public void addNewString(String s){
+
+    }
+    public void setNeighbours(List<Tecton> tectons){
+        
+    }
 }
