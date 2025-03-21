1196affdf41a6b7bb6f7d660cdf83f5f6f6052a4
Bánszky Koppány <kopibanszky@gmail.com>
2025-03-21 20:37:19 +0100

    smoll change

--- MushroomBody.java
+++ MushroomBody.java
@@ -6,6 +6,7 @@ public class MushroomBody {
 
     /**
      * A gombatestben található spórák számának csökkentése.
+     * @param spore - A spóra, amelyet törölni kell.
      */
     public void reduceSpore(Spore spore) {
         spores.remove(spore);
@@ -14,11 +15,15 @@ public class MushroomBody {
 
     /**
      * A gombatestben található spórák elszórása a környező tektonokra.
+     * A spórák számától függően szórja el a spórákat a környező tektonokra.
      */
-    public void scatter(Spore spore) {
+    public void scatter() {
         //melyik tektonon van a gomba? honnan lehet tudni?
         //melyik tektonokra szórja el a gombát? honnan lehet tudni?
+        //mennyi spórt szór szét?
         System.out.println("MushroomBody scatter");
+        //getNeighbour distance??????????
+        reduceSpore(spores.get(0));
     }
 
     /**
