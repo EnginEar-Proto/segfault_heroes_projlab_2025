628df2e6eba8439586106f2d616b051109c55e93
Bánszky Koppány <kopibanszky@gmail.com>
2025-03-21 20:25:26 +0100

    MushroomBody, MushroomString skeleton partial works. ton of wrong planning

--- MushroomBody.java
+++ MushroomBody.java
@@ -2,5 +2,33 @@ import java.util.List;
 
 public class MushroomBody {
     private List<MushroomString> strings;
-    //private List<> spores;
+    private List<Spore> spores;
+
+    /**
+     * A gombatestben található spórák számának csökkentése.
+     */
+    public void reduceSpore(Spore spore) {
+        spores.remove(spore);
+        System.out.println("MushroomBody reduceSpore");
+    }
+
+    /**
+     * A gombatestben található spórák elszórása a környező tektonokra.
+     */
+    public void scatter(Spore spore) {
+        //melyik tektonon van a gomba? honnan lehet tudni?
+        //melyik tektonokra szórja el a gombát? honnan lehet tudni?
+        System.out.println("MushroomBody scatter");
+    }
+
+    /**
+     * Fonal növesztése a gombatestből.
+     */
+    public void createNewString() {
+        MushroomString newString = new MushroomString();
+        strings.add(newString);
+        System.out.println("MushroomBody createNewString");
+        //melyik teknon van a gombatest? kene tárolni ez nagyon elbaszott így
+
+    }
 }
--- MushroomString.java
+++ MushroomString.java
@@ -1,6 +1,83 @@
 import java.util.List;
 
+
 public class MushroomString {
     private int length;
     private List<MushroomBody> mushroomBodies;
+
+    /**
+     * Gombatest hozzáadása a gombaszálhoz.
+     * @param mushroomBody
+     */
+    public void addMushroomBody(MushroomBody mushroomBody) {
+        mushroomBodies.add(mushroomBody);
+    }
+
+    /**
+     * Elvágja a gombaszálat.
+     * A gombaszálból 2 rész marad, ha mindkét oldalt van gombatest ami táblálja, egyébként a gombaszál egyik fele törlődik.
+     */
+    public void cut() {
+        System.out.println("MushroomString cut");
+        if(mushroomBodies.size() >= 2) {
+            System.out.println("MushroomString cut in half");
+        } else {
+            System.out.println("MushroomString cut in one");
+        }
+        //Honnan tudjuk melyik tektonnál vágja el a rovar a szálat?
+        //Mi alapján van a szál hossza?
+    }
+
+    /**
+     * A gombaszál eltűnik a tektonról. A fonál kettészakad, ha csak a egyik oldalon van gombatest, akkor a másik eltűnik.
+     * @param tecton, amiről eltűnik a gombaszál.
+     */
+    public void stringDisappear(Tecton tecton) {
+        System.out.println("MushroomString disappear");
+        if(mushroomBodies.size() >= 2) {
+            System.out.println("MushroomString disappear in half");
+        } else {
+            System.out.println("MushroomString disappear in one");
+        }
+        //itt kéne vmi út felderítés vagymi az isten ez? sztem nagypn el van baszarintva a fonalak kezelése
+    }
+
+    /**
+     * A gombaszál nő a paraméterként kapott tektonra.
+     * @param tecton
+     */
+    public void growTo(Tecton tecton) {
+        System.out.println("MushroomString grow to Tecton");
+        tecton.stringGrowFrom(); //honnan? wtf
+        if(tecton.haveSpore()) {
+            System.out.println("Can grow more");
+        } else {
+            System.out.println("Can't grow more");
+        }
+    }
+
+    /**
+     * A gombaszál elágazik, ha a tecton, amire ágazik elég nagy.
+     */
+    public void branchOut() {
+        System.out.println("MushroomString branch out");
+        //honnan tudjuk, hogy melyik tektonnál ágazik el a szál?
+        //honnan tudjuk h melyik tektonra ágazik el?
+
+    }
+
+    /**
+     * Gombszál létrehozása.
+     */
+    public void createString() {
+        //lehet tudni? nincs ilyen a szekvenci diagrammokon
+    }
+
+    /**
+     * A gombaszál nő a paraméterként kapott tektonra.
+     * @param tecton
+     */
+    public void grow(Tecton tecton) {
+        //??? ilyen sincs
+    }
 }
