3de9b2f8924568402033984f9580cd22822c5e0e
Bánszky Koppány <kopibanszky@gmail.com>
2025-03-22 13:32:29 +0100

    MushroomString implemented

--- MushroomString.java
+++ MushroomString.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -6,11 +7,18 @@ public class MushroomString {
     private int length;
     private List<MushroomBody> mushroomBodies;
     private List<Tecton> tectons;
+    private List<Map<Tecton, Tecton>> branches; //Key: from, Value: to
 
-    MushroomString(int length, List<MushroomBody> mushroomBodies, List<Tecton> tectons) {
+    private void removeTecton(Tecton tecton) {
+        tectons.remove(tecton);
+        branches.remove(tecton);
+    }
+
+    public MushroomString(int length, List<MushroomBody> mushroomBodies, List<Tecton> tectons, List<Map<Tecton, Tecton>> branches) {
         this.length = length;
         this.mushroomBodies = mushroomBodies;
         this.tectons = tectons;
+        this.branches = branches;
     }
 
     /**
@@ -24,6 +32,11 @@ public class MushroomString {
     /**
      * Elvágja a gombaszálat.
      * A gombaszálból 2 rész marad, ha mindkét oldalt van gombatest ami táblálja, egyébként a gombaszál egyik fele törlődik.
+     * @param t1 - Az egyik tecton, amelyik a vágás egyik pontja.
+     * @param t2 - A másik tecton, amelyik a vágás másik pontja.
+     *           A tectonoknak szomszédosnak kell lenniük.
+     *           A vágás a két tekton között történik
+     *
      */
     public void cut(Tecton t1, Tecton t2) {
         if(t1.neighbourDistance(t2) != 1) {
@@ -65,23 +78,41 @@ public class MushroomString {
      * @param tecton, amiről eltűnik a gombaszál.
      */
     public void stringDisappear(Tecton tecton) {
-        System.out.println("MushroomString disappear");
-        if(mushroomBodies.size() >= 2) {
-            System.out.println("MushroomString disappear in half");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Is there mushroomBody on both end?[y/n]: ");
+        String input = scanner.nextLine();
+        int index = tectons.indexOf(tecton);
+        if(input.equals("y") && mushroomBodies.size() >= 2) {
+            List<Tecton> newTectons = tectons.subList(index + 1, tectons.size()-1);
+            tectons = tectons.subList(0, index);
+            MushroomString newString = new MushroomString(length - index, mushroomBodies, newTectons);
+            newTectons.forEach(t -> t.addNewString(newString));
+            newTectons.forEach(t -> t.removeString(this));
+            tecton.removeString(this);
+            System.out.println("MushroomString disappear");
         } else {
-            System.out.println("MushroomString disappear in one");
+            tectons.subList(index + 1, tectons.size()-1).forEach(t -> t.removeString(this));
+            tecton.removeString(this);
+            System.out.println("MushroomString disappear");
         }
-        //itt kéne vmi út felderítés vagymi az isten ez? sztem nagypn el van baszarintva a fonalak kezelése
     }
 
     /**
      * A gombaszál nő a paraméterként kapott tektonra.
-     * @param tecton
+     * @param toGrow - A tekton, amire a gombaszál nőni fog.
+     * @param from - A tekton, ahonnan a gombaszál nőni fog.
+     *             A két tektonnak szomszédosnak kell lennie.
      */
-    public void growTo(Tecton tecton) {
-        System.out.println("MushroomString grow to Tecton");
-        tecton.stringGrowFrom(); //honnan? wtf
-        if(tecton.haveSpore()) {
+    public void growTo(Tecton toGrow, Tecton from) {
+        if(from.neighbourDistance(toGrow) != 1) {
+            System.out.println("Can't grow MushroomString, tectons are not neighbours");
+            return;
+        }
+        toGrow.addNewString(this);
+        tectons.add(toGrow);
+        length++;
+
+        if(toGrow.haveSpore()) {
             System.out.println("Can grow more");
         } else {
             System.out.println("Can't grow more");
@@ -90,26 +121,26 @@ public class MushroomString {
 
     /**
      * A gombaszál elágazik, ha a tecton, amire ágazik elég nagy.
+     * @param toBranch - A tekton, amire a gombaszál ágazni fog.
+     * @param from - A tekton, ahonnan a gombaszál ágazni fog.
+     *                 A két tektonnak szomszédosnak kell lennie.
+     *                 A toBranch tecton méretének legalább 2-nek kell lennie.
+     *
      */
-    public void branchOut() {
-        System.out.println("MushroomString branch out");
-        //honnan tudjuk, hogy melyik tektonnál ágazik el a szál?
-        //honnan tudjuk h melyik tektonra ágazik el?
+    public void branchOut(Tecton toBranch, Tecton from) {
+        if(from.neighbourDistance(toBranch) != 1) {
+            System.out.println("Can't branch MushroomString, tectons are not neighbours");
+            return;
+        }
+        if(toBranch.canBranch()) {
+            MushroomString newString = new MushroomString(length++, mushroomBodies, List.of(toBranch), List.of(Map.of(toBranch, from)));
+            toBranch.addNewString(newString);
+            tectons.add(toBranch);
+            System.out.println("MushroomString branched out");
+        } else {
+            System.out.println("Can't branch MushroomString, tecton is not big enough");
+        }
 
     }
 
-    /**
-     * Gombszál létrehozása.
-     */
-    public void createString() {
-        //lehet tudni? nincs ilyen a szekvenci diagrammokon
-    }
-
-    /**
-     * A gombaszál nő a paraméterként kapott tektonra.
-     * @param tecton
-     */
-    public void grow(Tecton tecton) {
-        //??? ilyen sincs
-    }
 }
