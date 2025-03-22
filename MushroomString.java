a2c89b4bcb4861f1d78e8d507ee8a2f78efea0cf
Bánszky Koppány <kopibanszky@gmail.com>
2025-03-22 13:14:51 +0100

    string cut implemented

--- MushroomString.java
+++ MushroomString.java
@@ -1,9 +1,17 @@
 import java.util.List;
+import java.util.Scanner;
 
 
 public class MushroomString {
     private int length;
     private List<MushroomBody> mushroomBodies;
+    private List<Tecton> tectons;
+
+    MushroomString(int length, List<MushroomBody> mushroomBodies, List<Tecton> tectons) {
+        this.length = length;
+        this.mushroomBodies = mushroomBodies;
+        this.tectons = tectons;
+    }
 
     /**
      * Gombatest hozzáadása a gombaszálhoz.
@@ -17,15 +25,39 @@ public class MushroomString {
      * Elvágja a gombaszálat.
      * A gombaszálból 2 rész marad, ha mindkét oldalt van gombatest ami táblálja, egyébként a gombaszál egyik fele törlődik.
      */
-    public void cut() {
-        System.out.println("MushroomString cut");
-        if(mushroomBodies.size() >= 2) {
+    public void cut(Tecton t1, Tecton t2) {
+        if(t1.neighbourDistance(t2) != 1) {
+            System.out.println("Can't cut MushroomString, tectons are not neighbours");
+            return;
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Is there mushroomBody on both end?[y/n]: ");
+        String input = scanner.nextLine();
+        int leftIndex = tectons.indexOf(t1);
+        int rightIndex = tectons.indexOf(t2);
+        if(input.equals("y") && mushroomBodies.size() >= 2) {
+            if(leftIndex > rightIndex) {
+                int temp = leftIndex;
+                leftIndex = rightIndex;
+                rightIndex = length - temp;
+            }
+            List<Tecton> newTectons = tectons.subList(leftIndex + 1, tectons.size()-1);
+            tectons = tectons.subList(0, leftIndex);
+            MushroomString newString = new MushroomString(rightIndex, mushroomBodies, newTectons);
+            newTectons.forEach(t -> t.addNewString(newString));
+            newTectons.forEach(t -> t.removeStrin(this));
             System.out.println("MushroomString cut in half");
+
         } else {
+            if(leftIndex > rightIndex) {
+                int temp = leftIndex;
+                leftIndex = rightIndex;
+                rightIndex = temp;
+            }
+            tectons.subList(leftIndex + 1, tectons.size()-1).forEach(t -> t.removeString(this));
+            tectons = tectons.subList(0, leftIndex);
             System.out.println("MushroomString cut in one");
         }
-        //Honnan tudjuk melyik tektonnál vágja el a rovar a szálat?
-        //Mi alapján van a szál hossza?
     }
 
     /**
