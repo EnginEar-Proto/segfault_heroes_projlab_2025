1bcfb704569ce83968d7ffc1ced1fb1fbeb99fca
Réti András <retia24@gmail.com>
2025-04-23 10:53:01 +0200

    Main változtatása a teszteléshez, test0 txt fájlok

--- Main.java
+++ Main.java
@@ -2,16 +2,24 @@ import java.io.IOException;
 
 /**
  * A Main osztály felelős a program megfelelő elindulásáért.
+ * Argumentum megadása nélkül normál, konzolos kezelés lehetséges.
+ * 2 argumentum megadása esetén két fájl útvonalat vár a program, az elsőből olvassa a parancsokat, a másodikba írja a kimenetet.
 */
 public class Main{
     public static void main(String[] args){
         try {
-            IOHandler ioHandler = new IOHandler("console", "console");
+            IOHandler ioHandler;
+            if (args.length == 2) {
+                ioHandler = new IOHandler(args[0], args[1]);
+            }
+            else {
+                ioHandler = new IOHandler("console", "console");
+            }
             CommandParser commandParser = new CommandParser();
             GameManager gameManager = new GameManager(ioHandler, commandParser);
             commandParser.setGameManager(gameManager);
             boolean running = true;
-            while (running){
+            while (running) {
                 String command = ioHandler.readLine();
                 running = commandParser.executeCommand(command);
             }
